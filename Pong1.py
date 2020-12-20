5342f82bcc6d129d7a7fb45b0a4cb3aacb6d6a58
serSalas <sergiosalas@mi.unc.edu.ar>
2020-12-20 13:06:01 -0300

    Agrega prototipo de paleta

--- Pong1.py
+++ Pong1.py
@@ -10,7 +10,19 @@ ventana.bgcolor("black")
 ventana.setup(width=800, height=600)
 ventana.tracer(0)
 
+#Paleta A
+paleta_a = turtle.Turtle()
+paleta_a.speed(0)
+paleta_a.shape("square")
+paleta_a.color("white")
+paleta_a.penup()
+paleta_a.goto(-350, 0)
+
+#Paleta B
+
+#Pelota
 
 #Bucle principal del juego
 while True:
     ventana.update()
+
